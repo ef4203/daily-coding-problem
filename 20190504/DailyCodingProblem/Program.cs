e360184d9a31bf230028829e59ee36f5634858d4
Elias Frank <32647755+ef4203@users.noreply.github.com>
2019-05-04 18:49:04 +0200

    Solved todays problem

---
+++ 20190504/DailyCodingProblem/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DailyCodingProblem
+{
+    class Program
+    {
+        static private ICollection<int> Problem(ICollection<int> a)
+        {
+            List<int> l = new List<int>();
+
+            for (int i = 0; i < a.Count; i++)
+            {
+                List<int> x = new List<int>((List<int>)a);
+
+                x[i] = 1;
+                int product = 1;
+
+                foreach (var e in x)
+                {
+                    product *= e;
+                }
+
+                l.Add(product);
+            }
+
+            return l;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            List<int> a = new List<int> { 1, 2, 3, 4, 5 };
+
+            foreach (var e in Problem(a))
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.Read();
+        }
+    }
+}
